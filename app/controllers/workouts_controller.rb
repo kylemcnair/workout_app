e2e8748c00a4a29c78bf2cb0ee16fd22d150f20d
kylemcnair <kmcnair3@gmail.com>
2019-12-16 13:10:34 -0800

    added new record page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,4 +2,11 @@ class WorkoutsController < ApplicationController
   def index
     @workouts = Workout.all.order(created_at: 'desc')
   end
+  def new
+    @workout = Workout.new
+  end
+  def create
+    @workout = Workout.new(:title, :description)
+    @workout.save
+  end
 end
